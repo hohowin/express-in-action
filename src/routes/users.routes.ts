bc6e5f200283a0e27f0c4ab0a2b25032e56949cd
Howin Ho <ho1ho@yahoo.com>
2024-05-17 15:14:08 -0400

    crud

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,28 @@
 import { Router, Request, Response } from 'express';
+import { 
+  getUsersController, 
+  getUserByIdController,
+  createUserController,
+  updateUserController,
+  deleteUserController } from "../controllers/user.controller"
 
 // New Router instance
 const router = Router();
 
 // Users routes
-router.get('/', (_, res: Response) => {
-  res.status(200).send('Users route!');
-});
+router.get('/', (_, res: Response) => 
+  getUsersController(res));
 
-router.get('/:id', (req: Request, res: Response) => {
-  res.status(200).send(`User ${req.params.id} route!`);
-});
+router.get('/:id', (req: Request, res: Response) => 
+  getUserByIdController(req, res));
+
+router.post('/', (req: Request, res: Response) => 
+  createUserController(req, res));
+
+router.put('/', (req: Request, res: Response) => 
+  updateUserController(req, res));
+
+router.delete('/', (req: Request, res: Response) => 
+  deleteUserController(req, res));
 
 export default router;
