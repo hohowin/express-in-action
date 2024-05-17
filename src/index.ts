fecf7e517cc1fe7ad2b90438aedaba6b1963c0e0
Howin Ho <ho1ho@yahoo.com>
2024-05-17 10:58:26 -0400

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World")
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("tiny"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
